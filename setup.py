0cd303f4dca4d3858746d06275bc55b243f8674e
Gaurav Gupta <iamgauravg@gmail.com>
2019-03-27 12:59:21 -0700

    Refactor exploratory analysis of passenger survival predictions using ridge logistic regression

--- setup.py
+++ setup.py
@@ -52,6 +52,10 @@ setup(
 
     packages=['titanic'],
     install_requires=[
-        'pypandoc>=1.4'
+        'pypandoc>=1.4',
+        'pytest>=4.3.1',
+        'pytest-runner>=4.4',
     ],
+    setup_requires=['pytest-runner'],
+    tests_requires=['pytest'],
 )
